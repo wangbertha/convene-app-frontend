af1ca5200df204c68ba9f9f31f0a0da4770330e8
Edwin <edwincastaneda0801@gmail.com>
2024-10-31 11:19:37 -0500

    deleted pages folder and moved eventList to /components/events and moved the store folder to the src folder, also cahnged the imports.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { Provider } from "react-redux";
-import store from "../store/store.js";
+import store from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
