6b453868ad49df232ed74393af45c7658fe0fd12
Rogelio Martinez <rogejr89@gmail.com>
2024-10-31 22:20:32 -0400

    Created components and css file. added global colors to index.css

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Root from "./layout/root";
 import EventsList from "./components/events/EventList";
+import Event from "./components/events/Event";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "/Events", element: <EventsList /> }],
+    children: [
+      { path: "/Events", element: <EventsList /> },
+      { path: "/events/:id", element: <Event /> }
+    ],
   },
 ]);
 
