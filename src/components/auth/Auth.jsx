5e75bce791b63ec25edc0004f5844c88810d8a31
Edwin <edwincastaneda0801@gmail.com>
2024-11-01 15:39:05 -0500

    deleted extra lines made by me

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useLoginMutation, useRegisterMutation } from "./authSlice";
 import { useNavigate } from "react-router-dom";
-
 import "../../styles/Auth.css";
 
 function Auth() {
