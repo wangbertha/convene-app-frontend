e93dc86dd88b7c502b0a79bb128b758825869990
Edwin <edwincastaneda0801@gmail.com>
2024-11-01 13:58:39 -0500

    Added a switch button to event list waiting for userSlice

--- src/components/events/EventCard.jsx
+++ src/components/events/EventCard.jsx
@@ -2,9 +2,10 @@ import { useSelector } from "react-redux";
 import { selectToken } from "../auth/authSlice";
 import { useUpdateEventMutation } from "./eventsSlice";
 import { useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 
 import { Link } from "react-router-dom";
+import { useGetMeQuery } from "../chat/userSlice";
 
 export default function EventCard({ event }) {
   const date = event.startTime.slice(0, 10);
@@ -12,22 +13,22 @@ export default function EventCard({ event }) {
 
   const token = useSelector(selectToken);
 
+  const { data: user, isLoading, error } = useGetMeQuery();
+  console.log(user);
+
   //state for changing button classnames when going or not going
   const [attending, setAttending] = useState(false);
 
   /*Using use effect to only run the logic when the user changes or the attending users
    Change to run the code only when its needed*/
   useEffect(() => {
-    if (token) {
-      // Decode token to get user ID
-      //Extracts the id of the the decoded users token
-      const { id: userId } = jwtDecode(token);
-      const isAttending = event.attendingUsers?.some(
-        (user) => user.id === userId
+    if (user) {
+      const isAttending = user.attendingEvents?.some(
+        (attendingEvents) => attendingEvents.id === event.id
       );
       setAttending(isAttending);
     }
-  }, [token, event.attendingUsers]);
+  }, [user]);
 
   const [updateEvent] = useUpdateEventMutation();
 
@@ -46,9 +47,9 @@ export default function EventCard({ event }) {
   return (
     <li className="event-card">
       <div className="event-details-div">
-        <a href={event.url}>
+        <Link to={`/events/${event.id}`}>
           <h3 className="event-name">{event.name}</h3>
-        </a>
+        </Link>
         <p>
           <b>Date:</b> {date}
         </p>
@@ -59,26 +60,35 @@ export default function EventCard({ event }) {
           <b>Venue:</b> {event.venue}
         </p>
       </div>
-      {token && (
-        <div className="going-buttons">
-          <button
-            className={attending ? "selected-button" : "not-selected-button"}
-            onClick={() => toggleAttendance(true)}
-          >
-            Going
-          </button>
-          <button
-            className={!attending ? "selected-button" : "not-selected-button"}
-            onClick={() => toggleAttendance(false)}
-          >
-            Not going
-          </button>
-          <button className="info-button">
-            {" "}
-            <Link to={`/events/${event.id}`}>More info</Link>
-          </button>
-        </div>
-      )}
+      <div className="event-buttons">
+        {token && (
+          <div className="switch-container">
+            <button
+              className={
+                attending
+                  ? "switch-button left selected"
+                  : "switch-button left not-selected"
+              }
+              onClick={() => toggleAttendance(true)}
+            >
+              Going
+            </button>
+            <button
+              className={
+                !attending
+                  ? "switch-button right selected"
+                  : "switch-button right not-selected"
+              }
+              onClick={() => toggleAttendance(false)}
+            >
+              Not going
+            </button>
+          </div>
+        )}
+        <Link className="info-button" to={`/events/${event.id}`}>
+          More info
+        </Link>
+      </div>
     </li>
   );
 }
