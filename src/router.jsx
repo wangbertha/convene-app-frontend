387c74598b574a27e72f0c23a6aa8e688c830511
Rogelio Martinez <rogejr89@gmail.com>
2024-11-01 11:35:59 -0400

    hot fix on router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -2,9 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Root from "./layout/root";
 import EventsList from "./components/events/EventList";
-
 import Auth from "./components/auth/Auth";
-=======
 import Event from "./components/events/Event";
 
 
@@ -13,13 +11,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     children: [
-
       { path: "/events", element: <EventsList /> },
       { path: "/register", element: <Auth /> },
-=======
       { path: "/Events", element: <EventsList /> },
       { path: "/events/:id", element: <Event /> }
-
     ],
   },
 ]);
