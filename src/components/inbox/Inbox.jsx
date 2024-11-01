cbc19fa03520630071e5333b261cb861f9794f5d
beefchief <c.lang.1993@gmail.com>
2024-11-01 14:04:14 -0500

    create css file, add mock-up message elements, update router

--- src/components/inbox/Inbox.jsx
+++ src/components/inbox/Inbox.jsx
@@ -1,9 +1,16 @@
 //placeholder for inbox, more to come with socket-io code
+import "../../styles/Inbox.css";
 
 export default function Inbox() {
   return (
-    <p className="placeholder">
-      This is a placeholder for our messaging app. Your inbox will go here.
-    </p>
+    <>
+      <h2 className="placeholder">
+        This is a placeholder for our messaging app. Your inbox will go here.
+      </h2>
+      <p className="recieved">This is a test Received message</p>
+      <p className="sent">This is a test Received message</p>
+      <p className="recieved">This is a test Received message</p>
+      <p className="sent">This is a test Received message</p>
+    </>
   );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./layout/root";
 import EventsList from "./components/events/EventList";
 import Event from "./components/events/Event";
+import Inbox from "./components/inbox/Inbox";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,8 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       { path: "/Events", element: <EventsList /> },
-      { path: "/events/:id", element: <Event /> }
+      { path: "/events/:id", element: <Event /> },
+      { path: "/inbox/", element: <Inbox /> },
     ],
   },
 ]);
