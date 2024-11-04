53a8615b17d97459c9b22c9be6e3b117da16e6d8
Your Name <your_email@example.com>
2024-11-04 13:19:09 -0500

    updated footer

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        developer contact info | privacy policy | meet the team | find us on socials | FAQ
+      </p>
+    </footer>
+  );
+}
