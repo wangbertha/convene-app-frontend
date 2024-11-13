42ad83c46c3bac3708d078383287731522ebef80
Your Name <your_email@example.com>
2024-11-13 11:38:20 -0500

    modified files regarding BrowseProfiles

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/profiles': {
-        target: 'http://localhost:3000', // Correct backend port
+        target: 'http://localhost:3000', // Backend port
         changeOrigin: true,
         secure: false,
       },
