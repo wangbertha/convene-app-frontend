4077309e0273610110733404c2370c7970c81727
Your Name <your_email@example.com>
2024-11-14 21:20:39 -0500

    modified files regarding BrowseProfiles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './layout/NavBar';
 import Footer from './layout/Footer';
-import HomePage from './layout/Home';
 import BrowseProfile from './components/users/BrowseProfiles';
 import Auth from './components/auth/Auth';
 import './App.css';
