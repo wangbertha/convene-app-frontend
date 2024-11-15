8204b97add8090e854e92d72553891003acd35c5
beefchief-code <c.lang.1993@gmail.com>
2024-11-14 18:21:47 -0600

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -34,7 +34,10 @@ export default function Home() {
         <article className="right-card">
           <h2>About Convene</h2>
           <p>
-            Welcome to Convene, the newest app to find friends, that special someone, or a companion for a local event. Logged in users can add and edit information about themselves, add their interests, create a list of other liked users to connect with, one-to-one chat with other users, and add local activity types they might be interested in attending.
+            Welcome to Convene, the newest app to find friends, that special someone, or a companion for a local event. 
+            Logged in users can add and edit information about themselves, add their interests, create a list of other 
+            liked users to connect with, one-to-one chat with other users, and add local activity types they 
+            might be interested in attending.
           </p>
         </article>
         <article className="left-card">
